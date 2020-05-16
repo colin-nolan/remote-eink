6dc921cfea97baa8072c10c47a8bfe287ccdd168
Colin Nolan <cn580@alumni.york.ac.uk>
2020-05-16 19:33:21 +0100

    Fixes and adds test for current-image endpoint.

--- remote_eink/api/display/current_image.py
+++ remote_eink/api/display/current_image.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Dict
 
 from remote_eink.api.display._common import display_id_handler, ImageSchema
 from remote_eink.display.controllers import DisplayController
@@ -6,8 +7,9 @@ from remote_eink.display.controllers import DisplayController
 
 @display_id_handler
 def search(display_controller: DisplayController):
-    images = display_controller.list_images()
-    return ImageSchema(only=["identifier"]).dump(images, many=True), HTTPStatus.OK
+    if display_controller.current_image is None:
+        return None, HTTPStatus.NOT_FOUND
+    return ImageSchema(only=["identifier"]).dump(display_controller.current_image), HTTPStatus.OK
 
 
 @display_id_handler
@@ -17,12 +19,11 @@ def delete(display_controller: DisplayController):
 
 
 @display_id_handler
-def delete(display_controller: DisplayController):
-    display_controller.clear()
-    return "Image cleared", HTTPStatus.OK
-
-
-@display_id_handler
-def put(display_controller: DisplayController, imageId: str):
-    display_controller.display(imageId)
-    return f"Image displayed: {imageId}", HTTPStatus.OK
+def put(display_controller: DisplayController, body: Dict):
+    image_id = body.get("id")
+    if image_id is None:
+        return f"\"id\" field missing: {body}", HTTPStatus.BAD_REQUEST
+    if display_controller.image_store.get(image_id) is None:
+        return f"Image not found: {image_id}", HTTPStatus.BAD_REQUEST
+    display_controller.display(image_id)
+    return f"Image displayed: {image_id}", HTTPStatus.OK
---
+++ remote_eink/tests/api/display/test_current_image.py
@@ -0,0 +1,47 @@
+from http import HTTPStatus
+
+import unittest
+
+from remote_eink.tests._common import TestBase
+from remote_eink.tests.storage._common import EXAMPLE_IMAGE_1
+
+
+class TestDisplayCurrentImage(TestBase):
+    """
+    Tests for the `/display/{displayId}/current-image` endpoint.
+    """
+    def test_get_when_none(self):
+        controller = self.create_dummy_display_controller()
+        result = self.client.get(f"/display/{controller.identifier}/current-image")
+        self.assertEqual(HTTPStatus.NOT_FOUND, result.status_code)
+
+    def test_get_when_set(self):
+        controller = self.create_dummy_display_controller()
+        controller.image_store.add(EXAMPLE_IMAGE_1)
+        controller.display(EXAMPLE_IMAGE_1.identifier)
+        result = self.client.get(f"/display/{controller.identifier}/current-image")
+        self.assertEqual(HTTPStatus.OK, result.status_code)
+        self.assertEqual(EXAMPLE_IMAGE_1.identifier, result.json["id"])
+
+    def test_set_to_existing_image(self):
+        controller = self.create_dummy_display_controller()
+        controller.image_store.add(EXAMPLE_IMAGE_1)
+        result = self.client.put(f"/display/{controller.identifier}/current-image",
+                                 json={"id": EXAMPLE_IMAGE_1.identifier})
+        self.assertEqual(HTTPStatus.OK, result.status_code)
+        self.assertEqual(EXAMPLE_IMAGE_1, controller.current_image)
+
+    def test_set_to_non_existent_image(self):
+        controller = self.create_dummy_display_controller()
+        result = self.client.put(f"/display/{controller.identifier}/current-image",
+                                 json={"id": EXAMPLE_IMAGE_1.identifier})
+        self.assertEqual(HTTPStatus.BAD_REQUEST, result.status_code)
+
+    def test_set_without_id(self):
+        controller = self.create_dummy_display_controller()
+        result = self.client.put(f"/display/{controller.identifier}/current-image", json={})
+        self.assertEqual(HTTPStatus.BAD_REQUEST, result.status_code)
+
+
+if __name__ == "__main__":
+    unittest.main()
