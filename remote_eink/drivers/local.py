48a5344d65574bb84970833e2d5a17cedbee780f
Colin Nolan <cn580@alumni.york.ac.uk>
2021-12-27 22:47:52 +0000

    Polished local display.

--- remote_eink/drivers/local.py
+++ remote_eink/drivers/local.py
@@ -1,9 +1,10 @@
 import io
 import logging
+import sys
 import tkinter as tk
 from multiprocessing import Process
 from multiprocessing.connection import Pipe, Connection
-from threading import Thread
+from threading import Thread, RLock
 
 import math
 
@@ -14,19 +15,21 @@ logger = logging.getLogger(__name__)
 try:
     from PIL import ImageTk, Image
 except ImportError:
-    # TODO
-    logger.error('"TODO" extra not installed')
+    logger.error('"localdisplay" extra not installed')
     raise
 
 
 class LocalDisplayDriver(BaseDisplayDriver):
     """
     Local display device driver.
+
+    Not thread safe.
     """
 
     def __init__(self):
         super().__init__()
         self._parent_connection, self._child_connection = Pipe()
+        self._window_process = None
 
     def _display(self, image_data: bytes):
         self._parent_connection.send(image_data)
@@ -43,7 +46,13 @@ class LocalDisplayDriver(BaseDisplayDriver):
     def start(self):
         # Mac (at least) requires UI updates to be made on the main thread. Running the display window as a separate
         # process to separate its need to be on the main.
-        Process(target=_run_local_screen_window, args=(self._child_connection,)).start()
+        self._window_process = Process(target=_run_local_screen_window, args=(self._child_connection,))
+        self._window_process.start()
+
+    def stop(self):
+        if self._window_process:
+            self._parent_connection.send(_LocalScreenWindow.KILL_BYTES)
+            self._window_process.join()
 
 
 def _run_local_screen_window(connection: Connection):
@@ -58,6 +67,8 @@ class _LocalScreenWindow:
 
     DEFAULT_PIXEL_WIDTH = 500
     DEFAULT_PIXEL_HEIGHT = 500
+
+    KILL_BYTES = b"+kill"
 
     @staticmethod
     def _create_window(pixel_width: int, pixel_height: int):
@@ -74,16 +85,35 @@ class _LocalScreenWindow:
     ):
         super().__init__()
         self._communication_pipe = communication_pipe
-        self._window = _LocalScreenWindow._create_window(pixel_width, pixel_height)
+        self._pixel_width = pixel_width
+        self._pixel_height = pixel_height
+        self._window = None
+        self._display_lock = RLock()
 
     def run(self):
+        with self._display_lock:
+            if self._window is None:
+                self._window = _LocalScreenWindow._create_window(self._pixel_width, self._pixel_height)
         Thread(target=self._handle_display_requests).start()
+        self._window.protocol("WM_DELETE_WINDOW", lambda: print("Close blocked", file=sys.stderr))
         self._window.mainloop()
+        print("main loop ended")
+
+    def stop(self):
+        with self._display_lock:
+            self._window.quit()
+            self._window = None
 
     def _handle_display_requests(self):
-        while True:
+        while self._window is not None:
             received = self._communication_pipe.recv()
-            self._display(received)
+            with self._display_lock:
+                if self._window is not None:
+                    if received == _LocalScreenWindow.KILL_BYTES:
+                        self.stop()
+                    else:
+                        self._display(received)
+        print("requests end")
 
     def _display(self, image_data: bytes):
         for widget in self._window.winfo_children():
